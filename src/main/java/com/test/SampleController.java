95503f0f15f342a22f6f184bfbba91fe1af4fe00
CIDResearch-8 <f19ba051@gmail.com>
2020-10-27 12:05:25 +0900

    2020.10.27コミットその2

--- src/main/java/com/test/SampleController.java
+++ src/main/java/com/test/SampleController.java
@@ -1,18 +1,29 @@
 package com.test;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.*;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value="name", required=false, defaultValue="deer") String name, Model model) {
+        List<String> list = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E"));
+        model.addAttribute("name", list);
+        return "hello";
+    }
+
+    @RequestMapping("/start")
     @ResponseBody
-    String home() {
-        return "Hello World!";
+    String start() {
+        return "Starting java!";
     }
 
     public static void main(String[] args) throws Exception {
