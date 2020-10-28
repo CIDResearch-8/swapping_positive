2cb37dc438b4e3f46dabb5c8586c519929d83ff1
CIDResearch-8 <f19ba051@gmail.com>
2020-10-28 19:38:53 +0900

    2020.10.28コミット 送信フォームと送信結果を表示する機能を作成

--- src/main/java/com/test/SampleController.java
+++ src/main/java/com/test/SampleController.java
@@ -1,17 +1,20 @@
 package com.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.*;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
+
+    List<Post> posts = new ArrayList<>();
 
     @RequestMapping("/hello")
     public String hello(@RequestParam(value="name", required=false, defaultValue="deer") String name, Model model) {
@@ -21,9 +24,25 @@ public class SampleController {
     }
 
     @RequestMapping("/start")
-    @ResponseBody
-    String start() {
-        return "Starting java!";
+    String start(Model model) {
+        String str = "Starting java";
+        model.addAttribute("str", str);
+        return "start";
+    }
+
+    @GetMapping("/confirm")
+    public String confirm(Model model) {
+        model.addAttribute("post", new Post());
+        Post.nextPostId();
+        return "confirm";
+    }
+
+    @PostMapping("/confirm")
+    public String confirmSubmit(@ModelAttribute Post post, Model model) {
+        model.addAttribute("post", post);
+        posts.add(post);
+        model.addAttribute("posts", posts);
+        return "result";
     }
 
     public static void main(String[] args) throws Exception {
