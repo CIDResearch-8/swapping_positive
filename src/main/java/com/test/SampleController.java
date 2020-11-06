d2a6ac316487356e98f050798c8d02e3815c9561
CIDResearch-8 <f19ba051@gmail.com>
2020-11-06 12:01:58 +0900

    コミットテスト

--- src/main/java/com/test/SampleController.java
+++ src/main/java/com/test/SampleController.java
@@ -1,21 +1,77 @@
 package com.test;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public SampleController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(SampleController.class, args);
+    //./helloにアクセスされた時
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(value="name", required=false, defaultValue="deer") String name, Model model) {
+        List<String> list = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "E", "F", "G"));
+        //html(view)側へlistを"name"として同期する
+        model.addAttribute("name", list.get(0));
+        //hello.htmlを読み込む
+        return "hello";
+    }
+
+    //./startにアクセスされた時
+    @RequestMapping("/start")
+    String start(Model model) {
+        String str = "Starting java";
+        //html(view)側へstrを"str"として同期する
+        model.addAttribute("str", str);
+        //start.htmlを読み込む
+        return "start";
+    }
+
+    //./confirmにアクセスし、フォームを送信した時
+    @GetMapping("/confirm")
+    //今回は受け取る側になる
+    public String confirm(Model model) {
+        //html(view)側から取得した"post"をPostインスタンスとして生成
+        List<Post> postList = new ArrayList<>();
+        List<Map<String, Object>> dataList = jdbcTemplate.queryForList("SELECT * FROM TEST;");
+        for (Map<String, Object> data : dataList) {
+            Post post = new Post();
+            post.setPostId(Long.parseLong(data.get("id").toString()));
+            post.setName((String)data.get("name"));
+            post.setContent((String)data.get("content"));
+            postList.add(post);
+        }
+        //postIdを次番にするため実行
+        model.addAttribute("postList", postList);
+        //return文に書かれた値と同じ@PostMappingがある場合、そちらのメソッドに遷移する
+        return "result";
+    }
+
+    //./confirmにアクセスし、フォームを送信した後の遷移
+    @PostMapping("/confirm")
+    //時系列は@GetMapping → @PostMappingとなる
+    public String confirmSubmit(@RequestParam("name") String name, @RequestParam("content") String content) {
+        Post.nextPostId();
+        jdbcTemplate.update("INSERT INTO TEST (id, name, content) VALUES (?, ?, ?);", Post.getStaticPostId(), name, content);
+        return "redirect:/confirm";
     }
 }
