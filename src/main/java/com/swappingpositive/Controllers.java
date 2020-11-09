eeaa7ecec8ce1ebb9ea4f8b532413f6ab3a3e02c
CIDReserch-8 <f19ba051@gmail.com>
2020-11-09 15:54:56 +0900

    functionブランチの作成

---
+++ src/main/java/com/swappingpositive/Controllers.java
@@ -0,0 +1,15 @@
+package com.swappingpositive;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@Controller
+@EnableAutoConfiguration
+
+public class Controllers {
+
+}
---
+++ src/main/java/com/swappingpositive/Post.java
@@ -0,0 +1,40 @@
+package com.swappingpositive;
+
+public class Post {
+    private static long staticPostId;
+    private long postId;
+    private String name;
+    private String content;
+
+    public Post() {
+        setPostId(staticPostId);
+    }
+
+    public static void nextPostId() {
+        staticPostId++;
+    }
+
+    public long getPostId() {
+        return postId;
+    }
+
+    private void setPostId(long postId) {
+        this.postId = postId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
