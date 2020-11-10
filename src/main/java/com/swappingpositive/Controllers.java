17134b9f12f8bd5f294c6bcf8f3b7452701620ef
CIDResearch-8 <f19ba051@gmail.com>
2020-11-10 09:46:15 +0900

    DB操作をするための記述を追加した

--- src/main/java/com/swappingpositive/Controllers.java
+++ src/main/java/com/swappingpositive/Controllers.java
@@ -1,15 +1,20 @@
 package com.swappingpositive;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.*;
 
 @Controller
 @EnableAutoConfiguration
 
 public class Controllers {
 
+    //DB操作をするための処理
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public Controllers(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
