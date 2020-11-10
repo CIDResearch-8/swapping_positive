a27dcf0c634e23b4f80fb2c32020cf97306992b3
CIDResearch-8 <f19ba051@gmail.com>
2020-11-10 10:22:35 +0900

    ページ遷移の大本を作成

--- src/main/java/com/swappingpositive/Controllers.java
+++ src/main/java/com/swappingpositive/Controllers.java
@@ -4,10 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @EnableAutoConfiguration
-
 public class Controllers {
 
     //DB操作をするための処理
@@ -16,5 +16,41 @@ public class Controllers {
     @Autowired
     public Controllers(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // / -> index.html
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    // /login -> login.html
+    @RequestMapping("login")
+    public String login() {
+        return "login";
+    }
+
+    // /user/ 以下はUsersクラスを介する
+    @Controller
+    @RequestMapping("/user")
+    static class Users {
+
+        // /user/<ユーザーID> -> user/mypage.html
+        @RequestMapping("/{userId}")
+        public String mypage() {
+            return "user/mypage";
+        }
+
+        // /user/home -> user/home.html
+        @RequestMapping("/home")
+        public String home() {
+            return "user/home";
+        }
+
+        // /user/
+        @RequestMapping("/{commentID}")
+        public String comment() {
+            return "user/comment";
+        }
     }
 }
