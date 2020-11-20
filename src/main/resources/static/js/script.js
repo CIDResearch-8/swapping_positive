c048e74586d2a058857ad76dfeb7a8b64c21bc89
f19ba031 <f19ba031@gmail.com>
2020-11-20 10:26:52 +0900

    #4 ツイートごとに、文字数に応じて高さが変更。#4のissuesの部分を修正。 クラス："content"をクラス："text"に変更。（名前が変わっただけです）

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -4,11 +4,11 @@ _window.on('load',function(){
     //.blockのそれぞれの要素を操作する(以下、.blockはeの変数に入る)
     $('.block').each(function(i, e){
         //.nameの高さと.contentの高さを足し合わせる
-        var hei = $(e).children('.name').height() + $(e).children('.content').height() + 20;
-        
+        var height = $(e).children('.name').height() + $(e).children('.text').height();
+
         //合計値が50px以上であれば高さを変更する
-        if (50 < hei) {
-            $(e).css('height', hei + 'px');
+        if (50 < height) {
+            $(e).css('height', height + 20 + 'px');
         }
         else {
             $(e).css('height', 'auto');
@@ -20,11 +20,11 @@ _window.on('resize',function(){
     //.blockのそれぞれの要素を操作する(以下、.blockはeの変数に入る)
     $('.block').each(function(i, e){
         //.nameの高さと.contentの高さを足し合わせる
-        var hei = $(e).children('.name').height() + $(e).children('.content').height() + 20;
+        var height = $(e).children('.name').height() + $(e).children('.text').height() + 20;
         
         //合計値が50px以上であれば高さを変更する
-        if (50 < hei) {
-            $(e).css('height', hei + 'px');
+        if (50 < height) {
+            $(e).css('height', height + 'px');
         }
         else {
             $(e).css('height', 'auto');
