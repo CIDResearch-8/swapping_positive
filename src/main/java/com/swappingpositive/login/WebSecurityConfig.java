13bb382b254915d0778ce16e933dc8289c7ad188
f19ba051 <f19ba051@gmail.com>
2020-11-20 11:01:09 +0900

    configureメソッドの可読性を上げ、一部コードを修正

--- src/main/java/com/swappingpositive/login/WebSecurityConfig.java
+++ src/main/java/com/swappingpositive/login/WebSecurityConfig.java
@@ -28,50 +28,56 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http
-                    /*.headers()    現バージョンでは対応していない？
-                    .xssProtection()
-                    .frameOptions()
-                    .contentTypeOptions()
-                    .cacheControl()
-                    .and()*/
+                    .headers()
+                        .frameOptions()
+                            .and()
+                        .xssProtection()
+                            .and()
+                        .cacheControl()
+                            .and()
+                        .contentTypeOptions()
+                            .and()
+                        .cacheControl()
+                            .and()
+                        .and()
                     .authorizeRequests()
-                    // 認証対象外のパスを設定する
-                    .antMatchers("/", "/login", "/registration/**", "/css/**", "/js/**", "/img/**")
-                    // 上記パスへのアクセスを許可する
-                    .permitAll()
-                    // その他のリクエストは認証が必要
-                    .anyRequest().authenticated()
-                    .and()
+                        // 認証対象外のパスを設定する
+                        .antMatchers("/", "/login", "/registration/**", "/css/**", "/js/**", "/img/**")
+                        // 上記パスへのアクセスを許可する
+                        .permitAll()
+                        // その他のリクエストは認証が必要
+                        .anyRequest().authenticated()
+                        .and()
                     .formLogin()
-                    // ログインフォームのパス
-                    .loginPage("/")
-                    // ログイン処理のパス
-                    .loginProcessingUrl("/login")
-                    // ログイン成功時の遷移先
-                    .defaultSuccessUrl("/menu")
-                    // ログイン失敗時の遷移先
-                    .failureUrl("/login-error")
-                    // ログインフォームで使用するユーザー名のinput name
-                    .usernameParameter("userId")
-                    // ログインフォームで使用するパスワードのinput name
-                    .passwordParameter("password")
-                    .permitAll()
-                    .and()
+                        // ログインフォームのパス
+                        .loginPage("/")
+                        // ログイン処理のパス
+                        .loginProcessingUrl("/login")
+                        // ログイン成功時の遷移先
+                        .defaultSuccessUrl("/menu")
+                        // ログイン失敗時の遷移先
+                        .failureUrl("/login-error")
+                        // ログインフォームで使用するユーザー名のinput name
+                        .usernameParameter("userId")
+                        // ログインフォームで使用するパスワードのinput name
+                        .passwordParameter("password")
+                        .permitAll()
+                        .and()
                     .rememberMe()
-                    .tokenValiditySeconds(86400) // 1ヶ月（秒）
-                    .and()
+                        .tokenValiditySeconds(86400) // 1ヶ月（秒）
+                        .and()
                     .logout()
-                    // ログアウトがパス(GET)の場合設定する（CSRF対応）
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    // ログアウトがPOSTの場合設定する
-                    //.logoutUrl("/logout")
-                    // ログアウト後の遷移先
-                    .logoutSuccessUrl("/")
-                    // セッションを破棄する
-                    .invalidateHttpSession(true)
-                    // ログアウト時に削除するクッキー名
-                    .deleteCookies("JSESSIONID", "remember-me")
-                    .permitAll();
+                        // ログアウトがパス(GET)の場合設定する（CSRF対応）
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        // ログアウトがPOSTの場合設定する
+                        //.logoutUrl("/logout")
+                        // ログアウト後の遷移先
+                        .logoutSuccessUrl("/")
+                        // セッションを破棄する
+                        .invalidateHttpSession(true)
+                        // ログアウト時に削除するクッキー名
+                        .deleteCookies("JSESSIONID", "remember-me")
+                        .permitAll();
         }
 
         @Autowired
