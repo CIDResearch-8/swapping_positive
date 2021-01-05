6a7e9d29d3ef2654a2dbbd22edf0b190ece213dc
f19ba063 <f19ba063@gmail.com>
2021-01-05 11:17:13 +0900

    アルゴリズムの組込み

--- src/main/java/com/swappingpositive/CommentService.java
+++ src/main/java/com/swappingpositive/CommentService.java
@@ -1,5 +1,6 @@
 package com.swappingpositive;
 
+import com.swappingpositive.algorithm.StringChanger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -16,10 +17,13 @@ public class CommentService {
 
     public void save(CommentForm form, String userId) {
         form.emptyToNullField();
+
+        StringChanger s = new StringChanger();
+        s.readInString(form.getComment());
         //ツイートをSQLに登録
         dao.insert(new Comment(
                 userId,
-                form.getComment()));
+                s.printString()));
     }
 
     public Comment findById(int id) {
