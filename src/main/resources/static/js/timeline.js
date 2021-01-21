92f495d5e4c4cf752eb112885d0831dea941105c
f19ba051 <f19ba051@gmail.com>
2021-01-22 08:30:58 +0900

    コメント削除機能の追加(途中)/jQueryの追加

--- src/main/resources/static/js/timeline.js
+++ src/main/resources/static/js/timeline.js
@@ -7,7 +7,18 @@ Vue.component('comment-view', {
                     '<div class="media border border-primary bg-light" v-if="comment.replyParentId == null">' +
                         '<img class="d-flex align-self-start mr-3" :src="comment.iconUri" :href="comment.iconUri" height="50" weight="50" >' +
                         '<div class="media-body">' +
-                            '<h5 class="mt-0"><a class="text-dark" :href="\'/\' + comment.userId + \'/mypage\'">{{comment.username}}</a></h5>' +
+                            '<div class="d-flex">' +
+                                '<h5 class="p-2 flex-grow-1 mt-0"><a class="text-dark" :href="\'/\' + comment.userId + \'/mypage\'">{{comment.username}}</a></h5>' +
+                                '<div class="p-2 float-right">' +
+                                  '<button class="btn btn-link dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' +
+                                    '<span class="material-icons"> more_vert </span>' +
+                                  '</button>' +
+                                  '<div class="dropdown-menu" aria-labelledby="dropdownMenu1">' +
+                                    '<a class="dropdown-item" href="#!" :class="{disabled: comment.userId == this.loginUser}">Action</a>' +
+                                    '{{this.loginUser}} and {{comment.userId}}'+
+                                  '</div>' +
+                                '</div>' +
+                            '</div>' +
                             '<a class="text-dark" :href="\'/\' + comment.userId + \'/comment/\' + comment.commentId">' +
                                 '<p>{{comment.comment}}</p>' +
                             '</a>' +
@@ -29,10 +40,12 @@ var app = new Vue({
     data() {
         return {
             comments: [],
-            intervalId: undefined
+            intervalId: undefined,
+            loginUser: ""
        }
     },
     mounted() {
+        this.loginUserGet();
         this.repeatAllCommentGet();
         this.intervalId = setInterval(() => {
             if (document.visibilityState == 'visible') {
@@ -99,6 +112,17 @@ var app = new Vue({
         formattingDate: function(comment) {
             var newDate = new Date(comment.date);
             this.$set(comment, 'date', newDate.toLocaleString('ja-JP'));
+        },
+        loginUserGet: function() {
+            axios
+                .get('/rest-api/login-user-id/get')
+                .then(response => {
+                    this.userId = response.data;
+                    console.log('getting user id is' + this.userId);
+                })
+                .catch(err => {
+                    console.log('get error');
+            });
         }
     }
 })
