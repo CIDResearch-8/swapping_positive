121fe375b2771c17bef34c8dbe726f8dcc717a6d
f19ba051 <f19ba051@gmail.com>
2021-01-21 12:34:46 +0900

    マイページのコメント表示をタイムラインと統一

--- src/main/resources/static/js/mypage.js
+++ src/main/resources/static/js/mypage.js
@@ -5,15 +5,18 @@ Vue.component('comment-view', {
     template: '<div>' +
                 '<div class="comment-block" :key="comment.commentId">' +
                     '<div class="media border border-primary bg-light" v-if="comment.replyParentId == null">' +
-                        '<img class="d-flex align-self-start mr-3">' +
+                        '<img class="d-flex align-self-start mr-3" :src="comment.iconUri" :href="comment.iconUri" height="50" weight="50" >' +
                         '<div class="media-body">' +
-                            '<h5 class="mt-0">{{comment.userId}}</h5>' +
+                            '<h5 class="mt-0"><a class="text-dark" :href="\'/\' + comment.userId + \'/mypage\'">{{comment.username}}</a></h5>' +
                             '<a class="text-dark" :href="\'/\' + comment.userId + \'/comment/\' + comment.commentId">' +
                                 '<p>{{comment.comment}}</p>' +
                             '</a>' +
                             '<div>' +
-                                '<span class="material-icons">chat</span>' +
-                                '{{comment.replies.length}}' +
+                                '<a class="text-dark" :href="\'/\' + comment.userId + \'/comment/\' + comment.commentId">' +
+                                    '<span class="material-icons">chat</span>' +
+                                    '{{comment.replies.length}}' +
+                                '</a>' +
+                                '<span class="text-secondary float-right">{{comment.date}}</span>' +
                             '</div>' +
                         '</div>' +
                    '</div>' +
@@ -50,6 +53,9 @@ var app = new Vue({
                     this.comments = response.data;
                     this.comments.forEach(comment => {
                         this.allReplyCommentGet(comment);
+                        this.iconUrlGet(comment);
+                        this.usernameGet(comment);
+                        this.formattingDate(comment);
                     });
                     console.log('getting all comments');
                 })
@@ -68,6 +74,32 @@ var app = new Vue({
                 .catch(err => {
                     console.log(err.response);
             });
+        },
+        iconUrlGet: function(comment) {
+             axios
+                 .get('/rest-api/' + comment.userId + '/icon-uri/get')
+                 .then(response => {
+                     this.$set(comment, 'iconUri', response.data);
+                     console.log('getting icon uri');
+                 })
+                 .catch(err => {
+                     console.log(err.response);
+             });
+        },
+        usernameGet: function(comment) {
+             axios
+                 .get('/rest-api/' + comment.userId + '/username/get')
+                 .then(response => {
+                     this.$set(comment, 'username', response.data);
+                     console.log('getting username');
+                 })
+                 .catch(err => {
+                     console.log(err.response);
+             });
+        },
+        formattingDate: function(comment) {
+             var newDate = new Date(comment.date);
+             this.$set(comment, 'date', newDate.toLocaleString('ja-JP'));
         }
     }
 })
